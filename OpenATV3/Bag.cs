c2bf2573855aaa6e3081dbbb3fd2a40787a126e8
un-simp <uncraftablecreeper@gmail.com>
2026-06-23 13:55:50 +0100

    just enough plist for ATV

---
+++ OpenATV3/Bag.cs
@@ -0,0 +1,24 @@
+﻿using Claunia.PropertyList;
+
+namespace OpenATV3;
+
+public class Bag
+{
+    // just enough plist for the atv (basic replication of ATV trailers)
+    public void GenerateBag(string filePath)
+    {
+        // create root object
+        NSDictionary root = new NSDictionary();
+        root.Add("auth-type","http");
+        root.Add("enabled","YES");
+        root.Add("log-url","http://localhost:8080/");
+        root.Add("menu-title","test");
+        root.Add("merchant","test");
+        root.Add("root-url","http://localhost:8080/index.xml");
+        // optional js to add
+        NSDictionary js = new NSDictionary();
+        js.Add("javascript-url", "http://localhost:8080/index.js");
+        root.Add("vendor-gk-3", js);
+        PropertyListParser.SaveAsXml(root, new FileInfo(filePath));
+    }
+}
--- OpenATV3/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace OpenATV3;
-
-public class Class1
-{
-}

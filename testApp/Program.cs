b12bba7511db5464d0455d3a164dae5734583c69
un-simp <uncraftablecreeper@gmail.com>
2026-06-23 13:56:54 +0100

    gitignore

---
+++ testApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenATV3;
+namespace testApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // you can then define parts of the bag (name, etcetc) and it will auto populate
+        Bag bagGeneration = new Bag();
+        bagGeneration.GenerateBag("/Users/un/Downloads/bag.plist");
+    }
+}
